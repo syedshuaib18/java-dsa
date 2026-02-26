2bf90745a21b833fa61338a6b515ca09ee7d9e9d
syedshuaib18 <syedshuaibt7@gmail.com>
2026-02-26 09:38:23 +0530

    dsa practice

---
+++ src/MOUNTAIN.java
@@ -0,0 +1,26 @@
+
+
+public class MOUNTAIN {
+
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 5, 4, 3, 2, 1};
+        int max = arr[0];
+        int  ans=mountain( arr, max);
+        System.out.println(ans);
+
+    }
+
+    static int mountain(int[] arr, int max){
+        for (int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];}}
+        return max;
+
+    }}
+
+
+
+
+
+
+
